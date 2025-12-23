c2b4fc09972d5100673a69e0c8beee8ecd0dff17
Ivan Gordillo <gordillosolis@icloud.com>
2025-12-23 17:08:45 -0600

    Happy ? operator

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,44 @@
+use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::path::Path;
 
-fn main() {
-    println!("Hello, world!");
-    let file = File::open("data/port1.txt").expect("Un error al abrir el archivo");
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = File::open("data/port1.txt")?;
     let reader = BufReader::new(file);
-    let mut lines = reader.lines().filter_map(Result::ok).peekable();
 
-    let mut n_assets: i32 = 0;
+    let mut lines = reader.lines().filter_map(Result::ok);
 
-    while let Some(line) = lines.next() {
-        if n_assets == 0 {
-            n_assets = line.parse().expect("Linea invalida!");
-        }
+    let fisrt_line = lines.next().ok_or("Bad file")?;
 
+    let n_assets: usize = fisrt_line.trim().parse()?;
+
+    println!("Leyendo {} assets . . .", n_assets);
+
+    let mut assets: Vec<Asset> = Vec::with_capacity(n_assets);
+
+    for (i, line) in lines.take(n_assets).enumerate() {
         let asset_data: Vec<f64> = line
             .split_whitespace()
-            .filter_map(|x| x.parse().ok())
-            .collect();
+            .map(|s| s.parse::<f64>())
+            .collect::<Result<Vec<_>, _>>()?;
 
-        print!()
-
-        println!("{}", n_assets);
+        assets.push(Asset::new(i, asset_data[0], asset_data[1]));
     }
+
+    println!("{:?}", assets.first());
+
+    Ok(())
 }
 
 #[derive(Debug)]
 struct Asset {
-    id: i32,
+    id: usize,
     mean_return: f64,
     standard_deviation: f64,
 }
 
 impl Asset {
-    fn new(id: i32, mean_return: f64, standard_deviation: f64) -> Asset {
+    fn new(id: usize, mean_return: f64, standard_deviation: f64) -> Asset {
         Asset {
             id,
             mean_return,
